f268e8991aa75ca111a65013ef5760eb10cf6c81
Eddy Khoza <39723991+eddy-khoza@users.noreply.github.com>
2025-08-11 22:07:54 +0200

    First commit: Basic project setup created

---
+++ backend/BlogPostApp/src/test/java/com/eddy/BlogPostApp/BlogPostAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eddy.BlogPostApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPostAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
